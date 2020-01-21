fa607557291ae211c018a996041db4784c2a0471
sign09116 <57554727+sign09116@users.noreply.github.com>
2020-01-21 15:47:37 +0800

    腳本修正

--- AppProject_1220/Assets/Scripts/Scripts/Gamemanager.cs
+++ AppProject_1220/Assets/Scripts/Scripts/Gamemanager.cs
@@ -4,15 +4,16 @@ using UnityEngine.UI;
 public class Gamemanager : MonoBehaviour
 {
     [Header("INFO物件拉條")]
-    protected Scrollbar InfoScrollbar;
-    protected Image Info;
+    public Scrollbar InfoScrollbar;
+    public Image Info;
     [Header("要改變的圖片")]
     public Sprite[] InfoChangeImage;
     // Start is called before the first frame update
 
     void Start()
     {
-
+        InfoScrollbar = GameObject.Find("跑馬燈拉條").GetComponent<Scrollbar>();
+        Info = GameObject.Find("可改變宣傳圖片").GetComponent<Image>();
         Screen.SetResolution(1080, 1920, false);
         InvokeRepeating("fixedInfo", 5f, 5f);
 
@@ -32,6 +33,7 @@ public class Gamemanager : MonoBehaviour
     public IEnumerator ChangeInfo()
     {
         InfoScrollbar.value = 0;
+        yield return new WaitForSeconds(2f);
         for (int i = 0; i < 20; i++)
         {
 
